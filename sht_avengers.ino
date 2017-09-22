73303824dd52a56dd1879861842c84fc7dbd01da
なりたけいすけ <narita_keisuke@cyberagent.co.jp>
2017-09-22 12:56:32 +0900

    スタートのスピードもコントロール

--- sht_avengers.ino
+++ sht_avengers.ino
@@ -20,8 +20,8 @@ const int OUTPUT_C2 = 13;
 const int OUTPUT_L1 = 16;
 const int OUTPUT_L2 = 17;
 
-const int PWM_SPEED = 200;
-const int STEP = 40;
+const int PWM_SPEED = 150;
+const int STEP = 10;
 
 void setup() {
   Serial.begin(9600);
@@ -55,9 +55,11 @@ String readL = "stop";
 int speedA = PWM_SPEED;
 int speedB = PWM_SPEED;
 int speedC = PWM_SPEED;
+boolean useStep = false;
 
 void loop() {
   Serial.println("=====");
+  useStep = false;
 
   String newReadA = read("readA", INPUT_A1, INPUT_A2);
   String newReadB = read("readB", INPUT_B1, INPUT_B2);
@@ -69,48 +71,66 @@ void loop() {
   if (readA != newReadA) {
     if (newReadA == "stop") {
       if (speedA > STEP) {
+        useStep = true;
         speedA = speedA - STEP;
       } else {
         readA = newReadA;
       }
     } else {
       readA = newReadA;
-      speedA = PWM_SPEED;
+      if (speedA < PWM_SPEED) {
+        useStep = true;
+        speedA = speedA + PWM_SPEED;
+      }
     }
     send(readA, speedA, OUTPUT_A1, OUTPUT_A2, true);
   }
+
   if (readB != newReadB) {
     if (newReadB == "stop") {
       if (speedB > STEP) {
+        useStep = true;
         speedB = speedB - STEP;
       } else {
         readB = newReadB;
       }
     } else {
       readB = newReadB;
-      speedB = PWM_SPEED;
+      if (speedB < PWM_SPEED) {
+        useStep = true;
+        speedB = speedB + PWM_SPEED;
+      }
     }
     send(readB, speedB, OUTPUT_B1, OUTPUT_B2, true);
   }
+
   if (readC != newReadC) {
     if (newReadC == "stop") {
       if (speedC > STEP) {
+        useStep = true;
         speedC = speedC - STEP;
       } else {
         readC = newReadC;
       }
     } else {
       readC = newReadC;
-      speedB = PWM_SPEED;
+      if (speedC < PWM_SPEED) {
+        useStep = true;
+        speedC = speedC + PWM_SPEED;
+      }
     }
     send(readC, speedC, OUTPUT_C1, OUTPUT_C2, true);
   }
+
   if (readL != newReadL) {
     readL = newReadL;
     send(readL, 0, OUTPUT_L1, OUTPUT_L2, false);
   }
 
   delay(5);
+  if (useStep) {
+    delay(25);
+  }
   // delay(1000);
 }
 
@@ -128,7 +148,7 @@ String read(String label, int v1, int v2) {
       state = "reverse";  // 0, 1
     }
   }
-  Serial.println(label + ": " + state);
+  // Serial.println(label + ": " + state);
   return state;
 }
 
@@ -157,6 +177,10 @@ void send(String state, int speed, int out1, int out2, bool isPwm) {
     return;
   }
 
+  if (isPwm) {
+    analogWrite(out2, 0);
+  } else {
+    digitalWrite(out2, LOW);
+  }
   digitalWrite(out1, LOW);
-  digitalWrite(out2, LOW);
 }
